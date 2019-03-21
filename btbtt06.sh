d6cd7c77cdeea67ab4b055af62abf57351c7088e
LIN Yizhou <yizhou.lin@outlook.com>
2019-03-21 11:19:20 +0800

    bash

--- btbtt06.sh
+++ btbtt06.sh
@@ -3,9 +3,11 @@
 for i in $(seq 0 8)
 do
     systemd-cat -t btbtt06Thread$i python -u btbtt06Threads.py &
+    echo "$!"
 done
 
 for i in $(seq 0 4)
 do
     systemd-cat -t btbtt06Atts$i python -u btbtt06Atts.py &
+    echo "$!"
 done
